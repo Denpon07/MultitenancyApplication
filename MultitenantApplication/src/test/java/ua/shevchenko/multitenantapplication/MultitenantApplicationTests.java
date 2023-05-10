bc296e2c39f0ca802d29aca0e0cda81cb90cb546
Denys Shevchenko <denpon07@gmail.com>
2023-05-10 22:10:33 +0300

    CreateDiplomeProject

---
+++ MultitenantApplication/src/test/java/ua/shevchenko/multitenantapplication/MultitenantApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.shevchenko.multitenantapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitenantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
